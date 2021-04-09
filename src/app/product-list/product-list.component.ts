08f1a653998d10460bb5e0e087a050bb5fddcd8e
Jeffrey Alexander Diaz Perez <wowexperience26@bextsa.com>
2021-04-08 22:18:55 -0500

    Initial Commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  buy () {
+    window.alert('¡ Product successfully purchased !')
+  }
+
+  share() {
+    window.alert('¡ The product has been shared !');
+  }
+
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
